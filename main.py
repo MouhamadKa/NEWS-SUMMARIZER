c5ace44d136336ce97f7ee295befad160a002966
MouhamadKa <105647006+Mouhamad-Kasem@users.noreply.github.com>
2024-05-31 13:57:37 +0400

    Second Commit: Prepare all the Back-End Functions

--- main.py
+++ main.py
@@ -64,6 +64,146 @@ def get_news(topic):
         print("Error occured during api request,", e)
         
         
+class AsisstantManager:
+    thread_id = None
+    assistant_id = None
+    
+    def __init__(self, model : str = model):
+        self.client = client
+        self.model = model
+        self.assistant = None
+        self.thread = None
+        self.run = None
+        self.summary = None
+        
+        
+        # Retrieve existing assistant and thread if IDs are already exists
+        if self.assistant_id:
+            self.assistant = self.client.beta.assistants.retrieve(
+                assistant_id = self.assistant_id
+            )
+        if self.thread_id:
+            self.thread = self.client.beta.threads.retrieve(
+                thread_id = self.thread_id
+            )
+            
+            
+    def create_assistant(self, name, instructions, tools):
+        if not self.assistant:
+            self.assistant = self.client.beta.assistants.create(
+                name = name,
+                model = self.model,
+                instructions= instructions,
+                tools = tools
+            )
+            self.assistant_id = self.assistant.id
+            print(f"Assistant ID: {self.assistant_id}")
+
+    def create_thread(self):
+        if not self.thread:
+            self.thread = self.client.beta.threads.create()
+            self.thread_id = self.thread.id 
+            print(f"Thread ID: {self.thread_id}")   
+            
+    def add_message_to_thread(self, role, content):
+        if self.thread:
+            self.client.beta.threads.messages.create(
+                thread_id = self.thread_id,
+                role = role,
+                content = content
+            )
+            
+    def run_assistant(self, instructions):
+        if self.assistant and self.thread:
+            self.run = self.client.beta.threads.runs.create(
+                assistant_id = self.assistant_id,
+                thread_id = self.thread_id,
+                instructions = instructions
+            )
+            
+    def process_messages(self):
+        if self.thread:
+            messages = self.client.beta.threads.messages.list(thread_id= self.thread_id)
+            summary = []
+            
+            last_message = messages.data[0]
+            role = last_message.role
+            response = last_message.content[0].text.value
+            summary.append(response)
+            
+            self.summary = "\n".join(summary)
+            print(f"SUMMARY ------> {role.capitalize()}: ====> {response}")
+            # I don't know if I have to return the summary here or not 1:42:45
+
+            # We could have did this instead
+            # for msg in messages:
+            #     role = msg.role
+            #     content = msg.content[0].text.value
+            #     print(f"SUMMARY ------> {role.capitalize()}: ====> {content}")
+                
+    def call_required_functions(self, required_actions):
+        if not self.run:
+            return 
+        
+        tool_outputs = []
+        for action in required_actions["tool_calls"]:
+            func_name = action["function"]["name"]
+            arguments = json.loads(action["function"]["arguments"])
+            
+            if func_name == "get_news":
+                output = get_news(topic=arguments["topic"])
+                print(f"FUNCTION CALLING OUTPUT =====> {output}")
+                
+                finall_str = ''
+                for item in output:
+                    finall_str += "\n".join(item)
+
+                tool_outputs.append({
+                    "tool_call_id" : action["id"],
+                    "output" : finall_str
+                })
+                
+            else:
+                raise ValueError(f"Unknown Function: {func_name}")
+        
+        print("Submitting outputs back to the Assistant...")
+        self.client.beta.threads.runs.submit_tool_outputs(
+            thread_id= self.thread_id,
+            run_id= self.run.id,
+            tool_outputs= tool_outputs
+        )
+                
+    # for streamlit
+    def get_summary(self):
+        return self.summary
+                
+    def wait_for_completion(self):
+        if self.thread and self.run:
+            while True:
+                time.sleep(5)
+                run_status = self.client.beta.threads.runs.retrieve(
+                    thread_id = self.thread_id,
+                    run_id = self.run.id
+                )                
+                print(f"RUN STATUS:: {run_status.model_dump_json(indent=4)}")
+
+                if run_status.status == "completed":
+                    self.process_messages()
+                    break
+                
+                elif run_status.status == "requires_action":
+                    print("FUNCTION CALLING NOW...")
+                    self.call_required_functions(
+                        required_actions=run_status.required_action.submit_tool_outputs.model_dump()
+                    )
+
+    # Run the steps
+    def run_steps(self):
+        run_steps = self.client.beta.threads.runs.steps.list(
+            thread_id= self.thread_id,
+            run_id= self.run.id
+        )
+        print(f"Run Steps ===> {run_steps}")
 
 
 def main():
